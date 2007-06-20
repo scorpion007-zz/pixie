9d1f63b79aebb86f42d896e0225cad85e0c0e861
geohar <geohar@366fa382-4619-0410-88af-c27c993b82fa>
2007-06-20 18:40:42 +0000

    allow large geocaches

--- src/ri/rendererContext.cpp
+++ src/ri/rendererContext.cpp
@@ -1286,7 +1286,8 @@ void	CRendererContext::RiOptionV(char *name,int n,char *tokens[],void *params[])
 				options->maxBrickSize	*=	1000;								// Convert into bytes
 			optionCheck(RI_NUMTHREADS,			options->numThreads,				1,32,int)
 			optionCheck(RI_THREADSTRIDE,		options->threadStride,				1,32,int)
-			optionCheck(RI_GEOCACHEMEMORY,		options->geoCacheMemory,			0,100000,int)
+			optionCheck(RI_GEOCACHEMEMORY,		options->geoCacheMemory,			0,500000,int)
+				options->geoCacheMemory	*=	1000;								// Convert into bytes
 			optionCheckColor(RI_OTHRESHOLD,		options->opacityThreshold,			0,1)
 			optionCheckColor(RI_ZTHRESHOLD,		options->zvisibilityThreshold,		0,1)
 				options->geoCacheMemory	*=	1000;								// Convert into bytes
